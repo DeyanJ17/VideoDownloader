95af4c3a16d55018f446a18f69e68de94fc74ced
Vishal Kamalakrishnan <vishkamalk@gmail.com>
2023-01-05 20:26:26 -0500

    file and folder

--- video_downloader.py
+++ video_downloader.py
@@ -1,13 +1,29 @@
 from pytube import YouTube
 
+print("Welcome to the Youtube Downloader\nEnter the number of the option you would like to choose\n")
+choice = int(input("1). Give a video link to download single video\n2). Download all videos in download.txt"))
+if choice == 1:  
+  link = input("Put your youtube link here!! URL: ")
+  Download(link)
+elif choice == 2:
+    scan_txt_file()
+
+
+#downloads the given links and
 def Download(link):
-  youtubeObject = YouTube(link)
-  youtubeObject = youtubeObject.streams.get_highest_resolution()
+  video = YouTube(link)
+  video = video.streams.get_highest_resolution()
   try:
-      youtubeObject.download()
+      video.download("videos")
   except:
     print("There has been an error in downloading your youtube video")
   print("This download has completed!")
 
-link = input("Put your youtube link here!! URL: ")
-Download(link)
+def scan_txt_file():
+    with open("download.txt", "r") as file:
+      for link in file:
+          Download(link)
+    
+      
+
+      
